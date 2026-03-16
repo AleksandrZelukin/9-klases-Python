052877ae9aeceeb7c2567812a55c0f1df0cbbc8e
Aleksandr Zelukin <alexandr44@inbox.lv>
2026-03-16 10:50:38 +0200

    Update apps

---
+++ darbs_ar_failiem.py
@@ -0,0 +1,3 @@
+aa = open("darbs_ar_failiem.txt", "w", encoding="utf-8")
+aa.write("Sveiki, šis ir jauns fails!")
+aa.close()
--- vardnicas_atkartojums_9c.py
+++ vardnicas_atkartojums_9c.py
@@ -1,6 +1,6 @@
 talruna_gramata = {}
 print("Vārdu un numuru ievade:")
-print("Lai pabeigtu ievadi, ierakstiet 'stop' kā vārdu.")
+# print("Lai pabeigtu ievadi, ierakstiet 'stop' kā vārdu.")
 while True:
     print("""Ko gribam darīt? 
           1 - Ievadīt jaunu ierakstu, 
@@ -11,13 +11,26 @@ while True:
     if izvelne == "1":
         while True:
             vards = input("Ievadiet vārdu: ")
-            if vards == "stop":
-                break
+            # if vards == "stop":
+            #     break
             numurs = input("Ievadiet numuru: ")
             talruna_gramata[vards] = numurs
+            print(f"Ieraksts '{vards}: {numurs}' ir pievienots.")
+            f = open("talruna_gramata.txt", "a", encoding="utf-8")
+            f.write(f"{vards}: {numurs}\n")
+            f.close()
+            izvelne = input("Ja vēlaties ievadīt vēl vienu ierakstu? (1 - Jā, 2 - Skatīt visus ierakstus un talak): ")
+            if izvelne != "1":
+                break
     if izvelne == "2":
-        for key in talruna_gramata:
-            print(key, talruna_gramata[key])
+        # for key in talruna_gramata:
+        #     print(key, talruna_gramata[key])
+        # for vards, numurs in talruna_gramata.items():
+        #     print(f"{vards}: {numurs}")
+        f = open("talruna_gramata.txt", "r", encoding="utf-8")
+        print(f.read())
+        f.close()
+
     if izvelne == "3":
         vards = input("Ievadiet vārdu, kuru dzēst: ")
         if vards in talruna_gramata:
