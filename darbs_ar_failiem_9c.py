c032e208d46e4c0ad4a10e9e206cc31319161071
Aleksandr Zelukin <alexandr44@inbox.lv>
2026-03-25 14:07:41 +0200

    update apps

--- darbs_ar_failiem_9c.py
+++ darbs_ar_failiem_9c.py
@@ -1,10 +1,16 @@
-aa = open("darbs_ar_failiem_9c.txt", "a", encoding="utf-8")
-# aa.write("Sveiki, šis ir mans trešais darbs ar failiem Pythonā!")
-aa = print("Sveiki, šis ir mans trešais darbs ar failiem Pythonā!", file=aa)
-# aa.close()
+import json
 
+aa = {"name": "Janis", "age": 40, "city": "Rezekne"}
+bb = {"name": "Anna", "age": 35, "city": "Riga"}
+cc = {"name": "Petrs", "age": 25, "city": "Daugavpils"}
+dd = {"name": "Lina", "age": 30, "city": "Liepaja"}
+ee = {"pieci": (5,"five"), "desmit": (10,"ten"), "divdesmit": (20,"twenty")}
 
-aa = open("darbs_ar_failiem_9c.txt", "r", encoding="utf-8")
-content = aa.read()
-print(content)
-aa.close()
+with open("darbs_ar_failiem_9c.json", "w", encoding="utf-8") as f:
+    json.dump([aa, bb, cc, dd, ee], f)
+    
+with open("darbs_ar_failiem_9c.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+print(data)
+
+f.close()
